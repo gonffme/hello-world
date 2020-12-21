8b9e43d2fa4e2d59aa7fe270afac0de82370802d
gonffme <331678187@qq.com>
2020-12-21 04:07:32 +0000

    add a c pro

---
+++ beaut.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+//#include "conio.h"
+#include "stdlib.h"
+//#include "curses.h"
+
+int main(){
+//	initscr();
+	char a=176,b=219;
+	printf("%c%c%c%c%c%c\n",b,a,a,a,b);
+//	getch();
+//	endwin();
+	return 0;
+}
